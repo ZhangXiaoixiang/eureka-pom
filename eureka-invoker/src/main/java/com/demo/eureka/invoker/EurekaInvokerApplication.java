63fa340d5ba11cfc0fe2ff79c9041564227721b1
ZhangXiaoxiang <1090510535@qq.com>
2019-01-28 11:40:11 +0800

    初始化

--- eureka-invoker/src/main/java/com/demo/eureka/invoker/EurekaInvokerApplication.java
+++ eureka-invoker/src/main/java/com/demo/eureka/invoker/EurekaInvokerApplication.java
@@ -11,7 +11,7 @@ public class EurekaInvokerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaInvokerApplication.class, args);
         System.out.println("服务调用者: http://localhost:9000/router");
-        System.out.println("http://localhost:8080/person/10086");
+        System.out.println("测试不成功补救调试:http://localhost:8080/person/10086");
         System.out.println("服务调用者: http://localhost:9000/getPlayerInfoList");
 
     }
