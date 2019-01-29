383ce6f7f0ad989d86da9a8047580bbdd5ff8ad5
ZhangXiaoxiang <1090510535@qq.com>
2019-01-29 16:32:27 +0800

    修改了invoker,这是最终版

--- eureka-cloud-invoker/src/main/java/com/demo/eureka/invoker/EurekaInvokerApplication.java
+++ eureka-cloud-invoker/src/main/java/com/demo/eureka/invoker/EurekaInvokerApplication.java
@@ -10,8 +10,9 @@ public class EurekaInvokerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaInvokerApplication.class, args);
+        System.out.println("----------------------------没有加入监测的-----------------------------");
         System.out.println("服务调用者: http://localhost:9000/router");
-        System.out.println("服务列表: http://localhost:9000/router2");
+
         System.out.println("测试不成功补救调试:http://localhost:8080/person/10086");
         System.out.println("服务调用者: http://localhost:9000/getPlayerInfoList");
 
